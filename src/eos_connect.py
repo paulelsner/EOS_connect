aff280e59e69e252d5eb77acdba80f47d7adf3fa
ohAnd <15704728+ohAnd@users.noreply.github.com>
2025-04-01 08:07:31 +0200

    Update CI workflow to delete previous nightly images and tag with timestamp; refactor price fetching logic in get_prices_from_tibber and enhance logging in get_pv_forecast

--- src/eos_connect.py
+++ src/eos_connect.py
@@ -381,10 +381,7 @@ def get_prices_from_tibber(tgt_duration, start_time=None):
 
     if len(extended_prices) < tgt_duration:
         remaining_hours = tgt_duration - len(extended_prices)
-        if remaining_hours == 1:
-            extended_prices.append(extended_prices[-1])
-        else:
-            extended_prices.extend(prices[:remaining_hours])
+        extended_prices.extend(prices[:remaining_hours])
     logger.info("[PRICES] Prices from TIBBER fetched successfully.")
     return extended_prices
 
@@ -470,8 +467,20 @@ def get_pv_forecast(tgt_value="power", pv_config_name="default", tgt_duration=24
     # fix for time changes e.g. western europe then fill or reduce the array to 48 values
     if len(forecast_values) > tgt_duration:
         forecast_values = forecast_values[:tgt_duration]
+        logger.debug(
+            "[FORECAST] Day of time change %s values reduced to %s for %s",
+            request_type,
+            tgt_duration,
+            pv_config_name,
+        )
     elif len(forecast_values) < tgt_duration:
         forecast_values.extend([forecast_values[-1]] * (tgt_duration - len(forecast_values)))
+        logger.debug(
+            "[FORECAST] Day of time change %s values extended to %s for %s",
+            request_type,
+            tgt_duration,
+            pv_config_name,
+        )
     return forecast_values
 
 
